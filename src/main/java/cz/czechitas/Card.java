e12f2c88bf2a9f349bc2cd1c681149da8b84f7a7
Vladimír Kročák <vladimir.krocak@seznam.cz>
2020-10-25 10:04:12 +0100

    Implement solution

--- src/main/java/cz/czechitas/Card.java
+++ src/main/java/cz/czechitas/Card.java
@@ -1,18 +1,115 @@
 package cz.czechitas;
 
+import java.util.*;
+
 public class Card {
+
     // TODO: Přidejte attributy (int) hodnota a (String) barva (hodnota 7-14, barva _"Kule, Žaludy, Listy, Srdce"_)
+    private int hodnota;
+    private String barva;
 
-    // TODO: Přidejte konstruktor s parametry hodnota a barva
+    public Card(int hodnota, String barva) {
+        // TODO: Přidejte konstruktor s parametry hodnota a barva
+        // TODO zajistěte, že nelze nastavit špatné hodnoty. Pokud špatná hodnota zadána, tak nenastavovat.
+        // Nastav hodnotu karty pouze pokud je její hodnota 7-14
+        if (hodnota >= 7 && hodnota <= 14) {
+            this.hodnota = hodnota;
+        }
 
-    // TODO zajistěte, že nelze nastavit špatné hodnoty. Pokud špatná hodnota zadána, tak nenastavovat.
+        // BONUS: Ošetři chybnou hodnotu.
+        // Toto nebylo původně součástí zadání.
+        // Jak se vytváří a pracuje s vlastními vyjímkami jsme si zatím neukazovali
+
+//        else {
+//            // Vyhoď chybu pokud hodnota karty je mimo povolený rosah.
+//            throw new IllegalArgumentException("Hodnota '"+hodnota+"' nemůže být použita jako hodnota karty");
+//        }
+
+        if (Arrays.asList("Kule", "Žaludy", "Listy", "Srdce").contains(barva)) {
+            this.barva = barva;
+        }
+//        else {
+//            // Vyhoď chybu pokud barvy karty není validní.
+//            throw new IllegalArgumentException("Barva '" + barva + "' nemůže být použita jako jako hodnota barvy.");
+//        }
+
+    }
 
     // TODO: Přidejte gettry pro attributy hodnota a barva
 
+    public int getHodnota() {
+        return hodnota;
+    }
+
+    public String getBarva() {
+        return barva;
+    }
+
     // TODO: Přidejte metodu která vypíše hodnotu jako název karty (7-10, spodek, svršek, král, eso)
+
+    /**
+     * Vypiš hodnotu karty jako její jméno karty (11 -> spodek atd.)
+     *
+     * @return hodnotu karty jako její jméno karty (11 -> spodek atd.)
+     */
+    public String getCardName() {
+        if (this.hodnota == 14) {
+            return "eso";
+        } else if (this.hodnota == 13) {
+            return "král";
+        } else if (hodnota == 12) {
+            return "svršek";
+        } else if (hodnota == 11) {
+            return "svršek";
+        } else {
+            // Pokud je hodnota jíná než 11-14. Vypiš ji jako String
+            return String.valueOf(hodnota);
+        }
+    }
 
     // TODO: implementujte toString metodu
     // TODO: místo hodnoty vypisujte název karty
 
+    @Override
+    public String toString() {
+        return this.getCardName() + " " + barva;
+    }
+
     // TODO: implementujte equals metodu
+
+    @Override
+    public boolean equals(Object o) {
+        // Pokud porovnáváme kartu samu se sebou není třeba porovnávat jeho hodnoty.
+        if (this == o) {
+            return true;
+        }
+
+        // Pokud porovnávaný objekt není kartou, můžeme ukončit porovnávání a vráti `false`.
+        if (!(o instanceof Card)) {
+            return false;
+        }
+
+        // Převedeme porovnáváný objekt na kartu. Usnadní nám to přístup na jeho hodnoty.
+        Card card = (Card) o;
+
+        // Porovnáme hodnoty obou karet
+        if (hodnota != card.getHodnota()) {
+            return false;
+        } else {
+            // Při porovnání barev, porovnáváme hodnoty typu `String`.
+            // Typ `String` je ve skutečnosti objektový typ.
+            // Objekty nemůžeme porovnávat pomocí `==`.
+            // Při použití `==` u objektů zjišťujeme, zda jsou porovnávané objekty, jeden a ten samí objekt.
+            //
+            // Pro porovnání `String` (a objektů) obecně, musíme opětovně použít metodu `.equals()`
+            return barva.equals(card.getBarva());
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = hodnota;
+        result = 31 * result + barva.hashCode();
+        return result;
+    }
 }
--- src/main/java/cz/czechitas/SpousteciTrida.java
+++ src/main/java/cz/czechitas/SpousteciTrida.java
@@ -5,45 +5,74 @@ import java.util.*;
 public class SpousteciTrida {
 
     public static void main(String[] args) {
-        // Seznamy
-        //        String karata = "Zelená Sedma";
-        //        List<String> karty = Arrays.asList(
-        //                "Zelená sedma",
-        //                "Žaludová sedma",
-        //                "Srdcová sedma"
-        //        );
-        //
-        //        List<String> prazdnyBalicek = new ArrayList<>();
-        //        System.out.println(prazdnyBalicek);
-        //        System.out.println(prazdnyBalicek.size());
-        //        prazdnyBalicek.add("Žaludova desítka");
-        //        System.out.println(prazdnyBalicek);
-        //        System.out.println(prazdnyBalicek.size());
-        //
-        //        // <0; prazdnyBalicek.size())
-        //        prazdnyBalicek.remove(0);
-        //        System.out.println(prazdnyBalicek);
-        //        System.out.println(prazdnyBalicek.size());
-        //        prazdnyBalicek.remove(0);
-        //        for (int i = 0; i < prazdnyBalicek.size(); i++) {
-        //            prazdnyBalicek.get(i);
-        //            prazdnyBalicek.remove(i);
-        //        }
-
-        // TODO: Naimplementujte si nejprve funkcionalitu karty. Můžete experimentovat s
-        //        Card karta = new Card(7, "Zelena");
-        //        System.out.println(karta);
+        // TODO: Naimplementujte si nejprve funkcionalitu karty. Můžete experimentovat s jedinou kartoug
 
         // TODO: Naplňte balíček karet
-        // TODO: Přesuň vytváření balíčku karet do vlastní metody a použij
-        // TODO: Vypište obsah balíčku
+        List<Card> balicekKaret = createCardDeck();
+        System.out.println(balicekKaret);
 
-        // BONUS pro rychlé
+        // TODO: BONUS pro rychlé Zamíchejte balíček karet
+        shuffleCardDeck(balicekKaret);
+        System.out.println(balicekKaret);
+
+    }
+
+    /**
+     * Vytvoř balíček karet.
+     *
+     * @return balíček karet
+     */
+    public static List<Card> createCardDeck() {
+        // TODO: Naimplementujte si nejprve funkcionalitu karty. Můžete experimentovat s jedinou kartou
+        // TODO: Naplňte balíček karet
+        // TODO: Přesuň vytváření balíčku karet do vlastní metody a použij
+        // Vytvoř prázný seznam karet
+        List<Card> cardDeck = new ArrayList<>();
+
+        // Vygeneruj hodnoty od 7-14
+        for (int hodnota = 7; hodnota <= 14; hodnota++) {
+
+            // Vygeneruj barvy
+            for (String barva : Arrays.asList("Kule", "Žaludy", "Listy", "Srdce")) {
+                // For-each ciklus
+                // Prochází postupně celý seznam (kolekci)
+                // Syntaxe :
+                // for (Typ aktuálníPrvek seznamu : procházenýSeznam) { /* blok kódu */ }
+                cardDeck.add(new Card(hodnota, barva));
+            }
+        }
+        // Vrať připravený balíček karet
+        return cardDeck;
+    }
+
+    /**
+     * Zamíchej balíček karet
+     *
+     * @param cardDeck balíček karet, který bude zamíchán
+     */
+    public static void shuffleCardDeck(List<Card> cardDeck) {
         // TODO: Zamíchejte balíček karet
         // TODO: Přesuň míchání balíčku karet do vlastní metody a použij
+        // Připrav generátor náhodných čísel
+        Random generatorNahodnychCisel = new Random();
 
-// Nápověda
-//        Random generatorNahodnychCisel = new Random();
-//        generatorNahodnychCisel.nextInt(6);
+        // Zjisti si velikost balíčku
+        int deckSize = cardDeck.size();
+
+        // Náhodně zamíchej karty pojedné.
+        // Protože mícháme karty pojedné, provedeme jej vícekrát než je velikost balíčku,
+        // abychom dosahly dostatečného zamýchání.
+        for (int i = 0; i < deckSize * 20; i++) {
+            // Náhodně vyndej kartu z balíčku
+            Card removedCard = cardDeck.remove(generatorNahodnychCisel.nextInt(deckSize));
+            // Vráť kartu na konec seznamu
+            cardDeck.add(removedCard);
+        }
+
+        // Metoda nic nevrací.
+        // Balíček, který jsme míchaly byl předán jako parametr metody.
+        // POZOR: Objekty, které se předávají do metod jako parametry metody mohou být takto upraveny.
+        // Metoda ale by neměla měnit objekty, pokud to není jejím úkolem.
+        // Metoda by neměla mít takové to vedlejší efekty.
     }
 }
